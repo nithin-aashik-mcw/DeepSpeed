b1d74692c9f8647d4983d965fbf1cb485814f29b
nithin-aashik-mcw <nithin.aashik@multicorewareinc.com>
2026-09-14 20:48:32 -0700

    Torch URL

--- setup.py
+++ setup.py
@@ -325,7 +325,7 @@ else:
 
 start_time = time.time()
 
-setup(name='deepspeed-mcw',
+setup(name='deepspeed-mcw1',
       version=version_str,
       description='DeepSpeed library',
       long_description=readme_text,
